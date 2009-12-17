02e95fe314010874c4d18dfccdab2f1f8080c52b
lcy1986a <lcy1986a@7446e868-e16e-11de-b236-59c9e861d739>
2009-12-17 09:03:09 +0000

    添加了手动输入数据的功能

--- trunk/RStarTree/resource.h
+++ trunk/RStarTree/resource.h
@@ -42,15 +42,17 @@
 #define ID_32791                        32791
 #define ID_32793                        32791
 #define ID_32792                        32792
+#define ID_32794                        32792
 #define ID_MOUSEDRAG                    32793
 #define ID_DEMOSHOW                     32794
+#define ID_ADDDATA                      32795
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        131
-#define _APS_NEXT_COMMAND_VALUE         32792
+#define _APS_NEXT_COMMAND_VALUE         32794
 #define _APS_NEXT_CONTROL_VALUE         1009
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
